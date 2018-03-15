366d109e1d650fb2246eed4e95755e3e197cbe2b
Lyt99 <Lyt99>
2018-03-15 21:01:30 +0800

    change data folder

--- Tianma/Globals.cs
+++ Tianma/Globals.cs
@@ -1,14 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Tianma
 {
     class Globals
     {
         public static string VERSION = "0.0.1";
-        public static string DATA_PATH = "/storage/emulated/0/Tianma";
+        public static string DATA_PATH = System.IO.Path.Combine(UnityEngine.Application.persistentDataPath, "Tianma/");
         public static API.ConfigManager CONFIG_MANAGER = new API.ConfigManager("Tianma");
     }
 }
